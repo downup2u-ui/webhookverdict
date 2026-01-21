aa9b519431774bf00f305f8a66edd7b345bad1d1
apple <xiaoqingwang@126.com>
2026-01-21 19:01:57 +0800

    update

--- functions/shopify/webhook.ts
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context: any) {
-  const target = "https://webhookverdict-worker.downup2u.workers.dev/shopify/webhook";
-
-  return fetch(target, {
-    method: context.request.method,
-    headers: context.request.headers,
-    body: context.request.body,
-  });
-}
